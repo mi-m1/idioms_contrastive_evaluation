6d6a34b5a72f3d970c246683f9cbb2d19aedf3c1
mi-m1 <miziya46@gmail.com>
2024-10-10 17:45:18 +0100

    rerunning fewshot with new arrows prompt: gpts

---
+++ xshot_prompting_replicate_arrows/literal_gpt4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --account=dcs-acad6
+#SBATCH --reservation=dcs-acad6
+#SBATCH --time=1-00:00:00
+#SBATCH -o l_gpt4.out
+#SBATCH --mail-user=zmi1@sheffield.ac.uk
+#SBATCH --mail-type BEGIN
+#SBATCH --mail-type END
+#SBATCH --mail-type FAIL
+
+module load Anaconda3/2022.10
+module load CUDA/10.1.243
+source activate /mnt/parscratch/users/acq22zm/anaconda/.envs/semeval-t4
+export HF_HOME=/mnt/parscratch/users/acq22zm/.cache
+
+LD_LIBRARY_PATH=""
+
+python gpt_models_1shot.py \
+--model "gpt-4" \
+--eval_dataset "../prompting/dataset/literal_1032.csv" \
+--setting "literal" \
+--model_abr "gpt4" \
